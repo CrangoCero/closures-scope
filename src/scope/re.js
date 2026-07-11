3c8cca03ccbc2cde613b9681f66c2773b0b8a1de
Crango <cristian.gomez0887@gmail.com>
2026-07-11 13:22:30 -0500

    Reasignacin y redeclaración

---
+++ src/scope/re.js
@@ -0,0 +1,31 @@
+var firstName; // Undefined
+firstName = "Cristian"; // String
+console.log(firstName);
+
+var lastName = "Rangel"; // declarar/asignar
+lastName = "Gómez"; //reagsignar
+console.log(lastName);
+
+var secondName = "Mauricio"; //declarar/asignar
+var secondName = "Andrés"; //reasignar
+console.log(secondName);
+
+//let
+let fruit = "apple"; //declarar/asignar
+fruit = "kiwi"; //reasignar
+console.log(fruit);
+
+let fruit = "banana"; //declarar/asignar
+console.log(fruit);
+
+//const
+const animal = "dog"; //declarar/asignar
+animal = "cat"; //reasignar
+console.log(animal);
+
+const vehicle = []; //declarar
+vehicle.push("Car"); //reasignar
+console.log(vehicle);
+
+vehicle.pop(); //reasignar
+console.log(vehicle);

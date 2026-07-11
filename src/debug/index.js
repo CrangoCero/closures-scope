b8bb190e89cf5d31cbe9e6d5dd4d06c05e86fa2c
Crango <cristian.gomez0887@gmail.com>
2026-07-11 16:08:10 -0500

    debug

---
+++ src/debug/index.js
@@ -0,0 +1,12 @@
+var a = "Hello World!";
+
+function hello() {
+  let b = "Hello World!";
+  const c = "Hello World!";
+  debugger;
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+hello();
